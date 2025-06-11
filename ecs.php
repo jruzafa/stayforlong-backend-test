205cdaffdfc8d4ed34f5b23024d011c376078f36
Jose Ruzafa <jose.ruzafa@gmail.com>
2025-06-11 19:20:00 +0200

    chore: change ecs and add github action to force to failed github pipeline

--- ecs.php
+++ ecs.php
@@ -8,7 +8,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([__DIR__ . '/src']);
 
-    $ecsConfig->sets([CodingStyle::DEFAULT]);
+    $ecsConfig->sets([CodingStyle::ALIGNED]);
 
     $ecsConfig->skip([
         FinalClassFixer::class => [
